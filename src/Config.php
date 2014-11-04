ab97584913d87ad3679eaa5086442f504707304b
Remco Tolsma <info@remcotolsma.nl>
2014-11-04 14:11:57 +0100

    Moved MultiSafepay classes from the Pronamic iDEAL plugin to this library.

---
+++ src/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Title: MultiSafepay config
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_MultiSafepay_Config extends Pronamic_WP_Pay_GatewayConfig {
+	public $account_id;
+
+	public $site_id;
+
+	public $site_code;
+}
