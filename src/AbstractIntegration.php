911f01120d0739e21477a2066d394445c28d2744
Remco Tolsma <info@remcotolsma.nl>
2016-02-23 15:25:55 +0100

    Moving some code the connect library.

--- src/AbstractIntegration.php
+++ src/AbstractIntegration.php
@@ -11,14 +11,6 @@
  * @since 1.0.0
  */
 abstract class Pronamic_WP_Pay_Gateways_MultiSafepay_AbstractIntegration extends Pronamic_WP_Pay_Gateways_AbstractIntegration {
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_MultiSafepay_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_MultiSafepay_Config';
-	}
-
 	public function get_settings_class() {
 		return 'Pronamic_WP_Pay_Gateways_MultiSafepay_Settings';
 	}
