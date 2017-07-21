90fb2f81653e1148f6f6ed97ac6750d57bc51e19
Remco Tolsma <info@remcotolsma.nl>
2017-07-21 16:36:01 +0200

    Added support for first and last name.

--- src/Connect/Gateway.php
+++ src/Connect/Gateway.php
@@ -139,7 +139,8 @@ class Pronamic_WP_Pay_Gateways_MultiSafepay_Connect_Gateway extends Pronamic_WP_
 		$customer->locale       = $payment->get_locale();
 		$customer->ip_address   = Pronamic_WP_Pay_Server::get( 'REMOTE_ADDR', FILTER_VALIDATE_IP );
 		$customer->forwarded_ip = Pronamic_WP_Pay_Server::get( 'HTTP_X_FORWARDED_FOR', FILTER_VALIDATE_IP );
-		$customer->first_name   = $payment->get_customer_name();
+		$customer->first_name   = $payment->get_first_name();
+		$customer->last_name    = $payment->get_last_name();
 		$customer->email        = $payment->get_email();
 
 		$transaction = new Pronamic_WP_Pay_Gateways_MultiSafepay_Connect_Transaction();
