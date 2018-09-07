5ad04803b83f213487b0a7370798164235f8fbca
Reüel van der Steege <reuel@pronamic.nl>
2018-09-07 13:36:03 +0200

    Remove unused `has_feedback` and `amount_minimum`.

--- src/Connect/Gateway.php
+++ src/Connect/Gateway.php
@@ -48,8 +48,6 @@ class Gateway extends Core_Gateway {
 		);
 
 		$this->set_method( Gateway::METHOD_HTTP_REDIRECT );
-		$this->set_has_feedback( true );
-		$this->set_amount_minimum( 0 );
 		$this->set_slug( self::SLUG );
 
 		$this->client = new Client();
