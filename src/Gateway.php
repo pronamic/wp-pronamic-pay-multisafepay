eec27a50617586ee1d1b1b35163270ae81006f37
Reüel van der Steege <reuel@pronamic.nl>
2022-09-01 17:00:41 +0200

    Remove void payment method.

--- src/Gateway.php
+++ src/Gateway.php
@@ -111,11 +111,6 @@ class Gateway extends Core_Gateway {
 		$this->register_payment_method( new PaymentMethod( PaymentMethods::SANTANDER ) );
 		$this->register_payment_method( new PaymentMethod( PaymentMethods::SOFORT ) );
 
-		$payment_method_void = new PaymentMethod( PaymentMethods::VOID );
-		$payment_method_void->set_status( 'active' );
-
-		$this->register_payment_method( $payment_method_void );
-
 		// Client.
 		$this->client = new Client();
 
